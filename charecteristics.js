e56e50cb3a585306b4fd71118f5e0e3bf1ca9626
BoART96 <45164766+BoART96@users.noreply.github.com>
2019-02-14 16:11:26 +0200

    Delete charecteristics.js

--- charecteristics.js
+++
@@ -1,8 +0,0 @@
-function test65(x, y) 
-{
-  return ((x == 65 || y == 65) || (x + y == 65));
-}
-console.log(test65(65, 50))
-console.log(test65(20, 50))
-console.log(test65(20, 20))
-console.log(test65(20, 30))
